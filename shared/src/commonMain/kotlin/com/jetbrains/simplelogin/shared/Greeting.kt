c4188b8f4a24f55ab0018fdbd98083b8fd43f146
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-16 18:15:31 +0700

    Update common module

--- shared/src/commonMain/kotlin/com/jetbrains/simplelogin/shared/Greeting.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/simplelogin/shared/Greeting.kt
@@ -4,6 +4,9 @@ class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return """
+            KMM proof-of-concept
+            Running on ${platform.name}!
+        """.trimIndent()
     }
 }
